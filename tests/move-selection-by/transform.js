3d5b297574cf11a21a85e2614086706ce69ff50f
Soreine <soreine.plume@gmail.com>
2016-09-23 13:45:42 +0200

    Correctly expose moveSelectionBy

---
+++ tests/move-selection-by/transform.js
@@ -0,0 +1,23 @@
+const expect = require('expect');
+
+module.exports = function(plugin, state) {
+    const cursorBlock = state.document.getDescendant('_cursor_');
+    const offset = 2;
+    const transform = state.transform();
+    state = transform
+        .moveToRangeOf(cursorBlock)
+        .moveForward(offset)
+        .apply();
+
+    state = plugin.transforms
+        .moveSelectionBy(state.transform(), -1, -1)
+        .apply();
+
+    expect(state.startBlock.text).toEqual('Col 0, Row 0');
+    const selection = state.selection;
+    expect(selection.startKey).toEqual(selection.endKey);
+    // Keep same offset
+    expect(selection.startOffset).toEqual(offset);
+
+    return state;
+};

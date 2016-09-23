9210877e618d5d6d1a905d90ff64f886349ca991
Soreine <soreine.plume@gmail.com>
2016-09-23 13:45:25 +0200

    Test moveSelection

---
+++ tests/move-selection/transform.js
@@ -0,0 +1,23 @@
+const expect = require('expect');
+
+module.exports = function(plugin, state) {
+    const cursorBlock = state.document.getDescendant('_cursor_');
+    const offset = 2;
+    const transform = state.transform();
+    state = transform
+        .moveToRangeOf(cursorBlock)
+        .moveForward(offset)
+        .apply();
+
+    state = plugin.transforms
+        .moveSelection(state.transform(), 2, 2)
+        .apply();
+
+    expect(state.startBlock.text).toEqual('Col 2, Row 2');
+    const selection = state.selection;
+    expect(selection.startKey).toEqual(selection.endKey);
+    // Keep same offset
+    expect(selection.startOffset).toEqual(offset);
+
+    return state;
+};

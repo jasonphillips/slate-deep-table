a61a9f07fe3e219d678bc32f7e25734857d77e2d
Soreine <soreine.plume@gmail.com>
2016-09-27 10:44:54 +0200

    Test that transform are correctly undoable

---
+++ tests/undo-insert-table/transform.js
@@ -0,0 +1,20 @@
+const expect = require('expect');
+
+module.exports = function(plugin, state) {
+    const cursorBlock = state.document.getDescendant('_cursor_');
+    const transform = state.transform();
+
+    state = transform
+        .moveToRangeOf(cursorBlock)
+        .moveForward(6) // Cursor here: Before|After
+        .apply();
+
+    state = plugin.transforms.insertTable(state.transform()).apply();
+
+    state = state.transform().undo().apply();
+
+    // Back to previous cursor position
+    expect(state.startBlock.text).toEqual('BeforeAfter');
+
+    return state;
+};
---
+++ tests/undo-remove-row/transform.js
@@ -0,0 +1,17 @@
+const expect = require('expect');
+
+module.exports = function(plugin, state) {
+    const cursorBlock = state.document.getDescendant('_cursor_');
+    const transform = state.transform();
+
+    state = transform.moveToRangeOf(cursorBlock).apply();
+
+    state = plugin.transforms.removeRow(state.transform()).apply();
+
+    state = state.transform().undo().apply();
+
+    // Back to previous cursor position
+    expect(state.startBlock.text).toEqual('Col 1, Row 1');
+
+    return state;
+};

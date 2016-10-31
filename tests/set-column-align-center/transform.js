2f63e6a1de598f08c1cb67f512cb580ad8f37962
Mickael Daniel <daniel.mickael@gmail.com>
2016-10-31 16:36:45 +0100

    Update setColumnAlign to set align data on table (#21)

--- tests/set-column-align-center/transform.js
+++ tests/set-column-align-center/transform.js
@@ -1,7 +1,11 @@
 module.exports = function(plugin, state) {
-    const cursorBlock = state.document.getDescendant('_cursor_');
-    const transform = state.transform();
-    state = transform.moveToRangeOf(cursorBlock).apply();
+    let transform = state.transform();
 
-    return plugin.transforms.setColumnAlign(state.transform()).apply();
+    const cursorBlock = state.document.getDescendant('_cursor_1');
+    state = transform.moveToRangeOf(cursorBlock).apply();
+    transform = plugin.transforms.setColumnAlign(state.transform(), 'center');
+
+    const cursorBlock2 = state.document.getDescendant('_cursor_2');
+    state = transform.moveToRangeOf(cursorBlock2).apply();
+    return plugin.transforms.setColumnAlign(state.transform(), 'right').apply();
 };

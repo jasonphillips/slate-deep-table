ee7d529cb7d915d77d46c7224386f663b2345dcd
Samy Pessé <samypesse@gmail.com>
2016-11-30 13:49:44 +0100

    Update slate to 0.15.x

--- tests/schema-standard-table/transform.js
+++ tests/schema-standard-table/transform.js
@@ -3,6 +3,6 @@ const Slate = require('slate');
 module.exports = function(plugin, state) {
     const schema = new Slate.Schema(plugin.schema);
     return state.transform()
-        .normalizeWith(schema)
+        .normalize(schema)
         .apply();
 };
